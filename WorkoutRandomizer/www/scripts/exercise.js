5d79965a46f18f272e0b9c5f045f85a0d00c8669
Brandon Short <bshortdev@gmail.com>
2019-03-19 01:04:44 -0400

    Added the exercises class

---
+++ WorkoutRandomizer/www/scripts/exercise.js
@@ -0,0 +1,19 @@
+﻿function Exercise(name, muscle, max, exclude){
+    this.name = name;
+    this.muscle = muscle;
+    this.max = max;
+    this.exclude = exclude;
+}
+
+// Update the 1 Rep Max for an exercise based on repitions and weight lifted.
+function updateMax(exer, reps, weight) {
+    exer.max = weight * (1 + (0.025 * reps)); // O'Connor's equation
+}
+
+function getRepWeight(exer, reps) {
+    return exer.max / (1 + (0.025 * reps)); // Inverse of O'Connor's equation
+}
+
+function updateExclude(exer) {
+    exer.exclude = !exer.exclude;
+}
